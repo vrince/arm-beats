1c5900f8e43ba9145590d7477908392fb47528d9
Thomas Vincent <vrince@gmail.com>
2024-04-26 15:30:41 +0000

    feat: directly cross compile using go

--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@
 
 #set -e
 BEAT="${1:-filebeat}"
-TAG="${2}"
+ARCH="${2:-armv7l}"
+TAG="${3}"
 
 if [[ -z ${TAG} || ${TAG} == "master" ]] ; then
-    TAG="v7.16.2"
+    TAG="v8.11.4"
     echo "Using default tag(${TAG})"
 fi
 
 VERSION="${TAG:1}"
-ARCH=$(uname -m)
 SRC=$(pwd)
 
 echo "Setup"
@@ -19,7 +19,9 @@ unset GOROOT
 export GOPATH=$(pwd)/go/project
 export PATH="/usr/local/go/bin:${GOPATH}/bin:${PATH}"
 mkdir -p "${GOPATH}"
+
 go version
+go tool dist list | grep linux
 
 if [[ ! -d beats ]] ; then
     echo "Cloning elastic/beats git tag(${TAG})"
@@ -29,11 +31,13 @@ if [[ ! -d beats ]] ; then
     git describe --tags
 fi
 
+set -e
+
 echo "Building(${BEAT})"
 
 rm -rf "${GOPATH}/bin"
 cd "${SRC}/beats/${BEAT}"
-go get -v -a ./...
+env GOOS=linux GOARCH=arm go install -v -a ./...
 
 echo "Patching package"
 
@@ -46,6 +50,5 @@ cd "${SRC}"
 wget -q -N "https://artifacts.elastic.co/downloads/beats/${BEAT}/${SRC_ARCHIVE}.tar.gz"
 tar -xf "${SRC_ARCHIVE}.tar.gz"
 mv "${SRC_ARCHIVE}" "${DEST_ARCHIVE}"
-cp -f "${GOPATH}/bin/${BEAT}" "${DEST_ARCHIVE}/${BEAT}"
+cp -f "${GOPATH}/bin/linux_arm/${BEAT}" "${DEST_ARCHIVE}/${BEAT}"
 tar -czf "${DEST_ARCHIVE}.tar.gz" "${DEST_ARCHIVE}"
-rm "${SRC_ARCHIVE}.tar.gz"
